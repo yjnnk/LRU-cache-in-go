924d2e5df5d1c230e16d2bddc7da673e2592c6b1
yuji <yujiricardo@gmail.com>
2024-07-16 16:03:21 -0300

    add test

--- LRUCache_test.go
+++ LRUCache_test.go
@@ -9,5 +9,18 @@ func TestNodeNotFound(t *testing.T) {
 	if(val != -1) {
 		t.Errorf("expected -1, got %v", val)
 	}
+}
 
+func TestNodeFound(t *testing.T) {
+	lru := NewLRUCache(2)
+	lru.cache["123"] = &Node{
+		key: "123",
+		value: "meu_valor",
+	}
+
+	val := lru.Get("123")
+
+	if(val != "meu_valor") {
+		t.Errorf("expected meu_valor, got %v", val)
+	}
 }
